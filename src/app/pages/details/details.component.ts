f3356b053dc5045d690fab1a5d4298cdc3b48208
Ayvenou <axel.yvenou@gmail.com>
2024-01-26 14:53:11 +0100

    details compenents + global styles for components and charts layout

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-details',
@@ -6,7 +10,54 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
+    public olympics$!: Observable<Olympic[]>;
+    public subscription: Subscription = new Subscription();
 
-    // TODO: implement
+    public countryId: number = 0;
+    public countryName: string = '';
 
+    public numberOfParticipations: number = 0;
+    public numberOfMedals: number = 0;
+    public numberOfAtletes: number = 0;
+
+    public chartData: any[] = [];
+
+    constructor(private OlympicService: OlympicService, private router: Router, private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+        this.olympics$ = this.OlympicService.getOlympics();
+        this.countryId = this.route.snapshot.params['id'];
+
+        this.subscription = this.olympics$.subscribe(data => {
+            const country = data.find(country => country.id == this.countryId);
+            if (country) {
+
+                this.countryName = country.country;
+
+                this.numberOfParticipations = country.participations.length;
+
+                this.numberOfMedals = country.participations
+                    .map(participation => participation.medalsCount)
+                    .reduce((acc, value) => acc + value, 0);
+
+                this.numberOfAtletes = country.participations
+                    .map(participation => participation.athleteCount)
+                    .reduce((acc, value) => acc + value, 0);
+
+                const medalsPerYear = country.participations
+                    .map(participation => {
+                        return {
+                            name: participation.year.toFixed(),
+                            value: participation.medalsCount
+                        }
+                    });
+
+                this.chartData = [{ name: this.countryName, series: medalsPerYear }];
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
